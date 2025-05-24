9f9fd5b4b967840a1aff65fd02a32f8e996c119b
Denis Ndiritu <denisndiritu1@gmail.com>
2025-05-24 12:54:56 +0300

    Created a function isDirector that accepts employee as an argument, and used as a type predicate. Created a function executeWork that accepts employee as an argument, and calls workDirectorTasks if the employee is a Director, and workTeacherTasks if the employee is a Teacher

--- 0x04-TypeScript/task_2/js/main.js
+++ 0x04-TypeScript/task_2/js/main.js
@@ -37,3 +37,16 @@ function createEmployee(salary) {
 console.log(createEmployee(200));
 console.log(createEmployee(1000));
 console.log(createEmployee('$500'));
+function isDirector(employee) {
+    return employee.workDirectorTasks !== undefined;
+}
+function executeWork(employee) {
+    if (isDirector(employee)) {
+        return employee.workDirectorTasks();
+    }
+    else {
+        return employee.workTeacherTasks();
+    }
+}
+console.log(executeWork(createEmployee(200)));
+console.log(executeWork(createEmployee(1000)));
--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -45,3 +45,18 @@ function createEmployee(salary: number | string): Teacher | Director {
 console.log(createEmployee(200));
 console.log(createEmployee(1000));
 console.log(createEmployee('$500'));
+
+function isDirector(employee:Director|Teacher): employee is Director{
+    return(employee as Director).workDirectorTasks !== undefined;
+}
+
+function executeWork(employee: Director | Teacher):string{
+    if(isDirector(employee)){
+        return employee.workDirectorTasks();
+    }else{
+        return employee.workTeacherTasks();
+    }
+}
+
+console.log(executeWork(createEmployee(200)));
+console.log(executeWork(createEmployee(1000)));
